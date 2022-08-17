f9ac1a755b29e0bafd53b29b247f4f49852bbea0
Goga <gogaimerlishvili1@gmail.com>
2022-08-18 03:10:34 +0400

    added sidebar comp with icons && styles

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,75 @@
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import {
+  faGithub,
+  faLinkedin,
+  faFacebook,
+} from '@fortawesome/free-brands-svg-icons'
+import './index.scss'
+
+const Sidebar = () => (
+  <>
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul className="ul">
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/goga-imerlishvili-1984a622a/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#808080" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/GogaImerlishvili"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#808080" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/gimerlishvili5"
+          >
+            <FontAwesomeIcon icon={faFacebook} color="#808080" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </>
+)
+
+export default Sidebar
