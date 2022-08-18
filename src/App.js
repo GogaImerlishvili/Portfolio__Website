72fed0b437e7bb173c6ca4b467956ae62195d702
Goga <gogaimerlishvili1@gmail.com>
2022-08-19 03:10:14 +0400

    added new route for contact comp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import About from './components/About'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   )
