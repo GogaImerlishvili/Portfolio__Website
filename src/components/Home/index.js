35c92eaccf78c73f10fb74474c415b162f02509c
Goga <gogaimerlishvili1@gmail.com>
2022-08-18 18:32:20 +0400

    used hooks for animate words

---
+++ src/components/Home/index.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react'
+import LogoTitle from '../../assets/images/logo-search-grid-1x.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['G', 'o', 'g', 'a']
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img className="logo" src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer / JavaScript / ReactJS</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
